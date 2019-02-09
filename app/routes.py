ccc9889a37a3a851db084470baea428141bc0ec5
Nikolai <imbpp123@gmail.com>
2019-02-09 23:00:25 +0400

    Fix: missunderstanding of description

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import request, json
 import statistics
 
+from sqlalchemy.sql import func
+
 from app import app, db
 from app.models import Measurand
 
@@ -14,10 +16,15 @@ def get_response(data, status):
     return response
 
 
+@app.route('/stations/<string:station>/overcome')
+def stations_overcome(station: str):
+    pass
+
+
 @app.route('/station/<string:station>/variable/<string:variable>')
 def stations_stats(station: str, variable: str):
     def get_real_measure(value: str):
-        result = ['sum', 'mean', 'max', 'min', 'median']
+        result = ['sum', 'mean', 'max', 'min']
         if value != 'all' and value in result:
             result.clear()
             result.append(value)
@@ -30,26 +37,27 @@ def stations_stats(station: str, variable: str):
     if hasattr(Measurand, variable) is not True:
         return get_response({'error': 'Field does not exits'}, 400)
 
-    result = Measurand.query \
-        .filter_by(id_entity=station)\
+    measurand_attr = getattr(Measurand, variable)
+
+    result = db.session.query(
+            Measurand.id_entity,
+            func.avg(measurand_attr).label('mean'),
+            func.min(measurand_attr).label('min'),
+            func.max(measurand_attr).label('max'),
+            func.sum(measurand_attr).label('sum'),
+        )\
+        .group_by(Measurand.id_entity) \
         .filter(Measurand.time_instant > timestamp_start) \
         .filter(Measurand.time_instant < timestamp_stop) \
         .all()
 
     data = []
-    for measurement in result:
-        data.append(getattr(measurement, variable))
+    for item in result:
+        new_station_data = {
+            'station': item.id_entity
+        }
+        for val in measure:
+            new_station_data[val] = getattr(item, val)
+        data.append(new_station_data)
 
-    maths = {
-        'sum': sum(data),
-        'mean': statistics.mean(data),
-        'max': max(data),
-        'min': min(data),
-        'median': statistics.median(data)
-    }
-
-    result = {}
-    for measure_type in measure:
-        result[measure_type] = maths[measure_type]
-
-    return get_response(result, 200)
+    return get_response(data, 200)
