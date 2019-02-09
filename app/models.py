12b06babc63804e441add59bdb60ee44b0a139e8
Nikolai <imbpp123@gmail.com>
2019-02-09 20:54:23 +0400

    Upload data from csv file to database using console.py command

--- app/models.py
+++ app/models.py
@@ -1,4 +1,30 @@
+import datetime
+from sqlalchemy.orm import validates
 from app import db
+
+
+def validate_float(key, value):
+    try:
+        value = float(value)
+    except ValueError:
+        raise ValueError("Field %s has wrong value: %s" % (key, value))
+    return value
+
+
+def validate_datetime(key, value):
+    def try_to_convert(date_str: str, date_format: str):
+        try:
+            date_result = datetime.datetime.strptime(date_str, date_format)
+        except ValueError:
+            date_result = False
+        return date_result
+
+    result = try_to_convert(value, '%Y-%m-%d %H:%M:%S.%f')
+    if result is False:
+        result = try_to_convert(value, '%Y-%m-%d %H:%M:%S')
+    if result is False:
+        raise ValueError("Field %s has wrong value: %s" % (key, value))
+    return result
 
 
 class Measurand(db.Model):
@@ -11,6 +37,47 @@ class Measurand(db.Model):
     pm2_5 = db.Column(db.Float)
     time_instant = db.Column(db.DateTime)
     created_at = db.Column(db.DateTime)
+    updated_at = db.Column(db.DateTime)
+
+    @validates('id')
+    def validate_id(self, key, value):
+        try:
+            value = int(value)
+        except ValueError:
+            raise ValueError("Field %s has wrong value: %s" % (key, value))
+        return value
+
+    @validates('so2')
+    def validate_so2(self, key, value):
+        return validate_float(key, value)
+
+    @validates('no2')
+    def validate_no2(self, key, value):
+        return validate_float(key, value)
+
+    @validates('o3')
+    def validate_o3(self, key, value):
+        return validate_float(key, value)
+
+    @validates('pm10')
+    def validate_pm10(self, key, value):
+        return validate_float(key, value)
+
+    @validates('pm2_5')
+    def validate_pm2_5(self, key, value):
+        return validate_float(key, value)
+
+    @validates('time_instant')
+    def validate_time_instant(self, key, value):
+        return validate_datetime(key, value)
+
+    @validates('updated_at')
+    def validate_updated_at(self, key, value):
+        return validate_datetime(key, value)
+
+    @validates('created_at')
+    def validate_created_at(self, key, value):
+        return validate_datetime(key, value)
 
     def __repr__(self):
-        return '<Measurand {}>'.format(self.id)
+        return '<Measurand {}>'.format(self.id, self.created_at, self.updated_at, self.time_instant)
--- console.py
+++ console.py
@@ -1,14 +1,37 @@
+from pprint import pprint
 import click
 import csv
-from app import app
+from app import app, db
+from app.models import Measurand
+
+
+def saveToDb(row):
+    try:
+        object = Measurand(
+            id=row[0],
+            id_entity=row[8],
+            so2=row[2],
+            no2=row[3],
+            o3=row[7],
+            pm10=row[4],
+            pm2_5=row[5],
+            time_instant=row[1],
+            created_at=row[9],
+            updated_at=row[10]
+        )
+        db.session.add(object)
+    except ValueError:
+        pass
+
 
 @click.command()
 @click.argument('filename')
 def upload(filename):
-    with open(filename, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    with open(filename) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quoting=0)
         for row in reader:
-            click.echo(', '.join(row))
+            saveToDb(row)
+    db.session.commit()
 
 
 if __name__ == '__main__':
